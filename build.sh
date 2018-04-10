a16f58339b6e765d9aee1d4c05e883fae565f460
Auston Stewart <auston@shedbuilt.net>
2018-04-09 18:33:10 -1000

    Initial commit of cmus

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+./configure prefix=/usr/local \
+            CONFIG_AAC=y \
+	    CONFIG_ALSA=y \
+	    CONFIG_FLAC=y \
+	    CONFIG_MAD=y \
+	    CONFIG_VORBIS=y \
+	    CONFIG_OPUS=n \
+            CONFIG_FFMPEG=n \
+            CONFIG_JACK=n \
+            CONFIG_PULSE=n \
+            CONFIG_SAMPLERATE=n \
+            CONFIG_ROAR=n &&
+make -j $SHED_NUMJOBS &&
+make DESTDIR="$SHED_FAKEROOT" install
